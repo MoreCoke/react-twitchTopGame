b1d785d59eed827d073ffb17f4b6729ab9344930
William Chen <seed45699@gmail.com>
2020-08-22 17:30:01 +0800

    初版完成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import "./reset.css";
-import "./styles.css";
+import React from 'react';
+import './reset.css';
+import './styles.css';
 import LiveCardItem from './LiveCardItem';
+import LiveCardItemEmpty from './LiveCardItemEmpty';
 import NavItem from './NavItem';
 
 export default class App extends React.Component {
@@ -9,30 +10,72 @@ export default class App extends React.Component {
     super(props);
     this.state = {
       isNavToggle: false,
-      topGameList: []
+      asyncTopGame: null,
+      asyncGameList: null,
+      currentGame: null,
+    };
+    this.header = {
+      headers: {
+        'Client-ID': 'bln1yehoadgj1i6k3l7vkccwp2s7ar',
+        Accept: 'application/vnd.twitchtv.v5+json',
+      },
+    };
+    this.url = {
+      topGameUrl: 'https://api.twitch.tv/kraken/games/top?limit=5',
+      gameListUrl: 'https://api.twitch.tv/kraken/streams/?limit=20&game=',
     };
   }
 
   componentDidMount() {
-    const topGameUrl = "https://api.twitch.tv/kraken/games/top?limit=5";
-    fetch(topGameUrl)
-    .then((response) => response.json())
-    .then((response)=> {
-      const { top } = response;
-      console.log('top', top);
-      
+    const { topGameUrl } = this.url;
+    fetch(topGameUrl, this.header)
+      .then((response) => response.json())
+      .then((response) => {
+        const { top } = response;
+        const arr = top.map((element) => element.game.name);
+        this.setState({
+          asyncTopGame: arr,
+        });
+    });
   }
+
+  handleTwitchGameList = (game) => {
+    let { gameListUrl } = this.url;
+    gameListUrl += game;
+    fetch(gameListUrl, this.header)
+      .then((response) => response.json())
+      .then((response) => {
+        const { streams } = response;
+        const arr = streams.map((element) => {
+          const {
+            channel: { display_name, logo, status, url: twitchUrl },
+            preview: { medium: gameBgc }
+          } = element;
+          return {
+            display_name,
+            logo,
+            status,
+            twitchUrl,
+            gameBgc
+          }
+        });
+        this.setState({
+          asyncGameList: arr,
+          currentGame: game
+        });
+    });
+  };
 
   handleToggleNavList = () => {
     this.setState({
       isNavToggle: !this.state.isNavToggle,
     });
-  }
-  
+  };
+
   render() {
-    const {isNavToggle} = this.state;
+    const { isNavToggle, asyncTopGame, asyncGameList, currentGame } = this.state;
     const navListShow = isNavToggle ? 'show' : '';
-    const navIconClose = isNavToggle ? 'close': '';
+    const navIconClose = isNavToggle ? 'close' : '';
     return (
       <React.Fragment>
         <nav>
@@ -46,14 +89,31 @@ export default class App extends React.Component {
               </div>
             </div>
           </div>
-          <ul className={`nav-list ${navListShow}`}></ul>
+          <ul className={`nav-list ${navListShow}`}>
+            {asyncTopGame &&
+              asyncTopGame.map((element) => (
+                <NavItem
+                  key={element}
+                  currentName={currentGame}
+                  name={element}
+                  handleTwitchGameList={this.handleTwitchGameList}
+                />
+              ))}
+          </ul>
         </nav>
         <section className="live">
           <h1 className="live-title">請先點選上方遊戲</h1>
           <h2 className="live-subtitle">
             Top 20 popular live streams sorted by current viewers
           </h2>
-          <div className="live-card__group"></div>
+          <div className="live-card__group">
+            {asyncGameList && asyncGameList.map(element => (
+              <LiveCardItem key={element.display_name} stream={element}/>
+            ))}
+            {
+              [...Array(4)].map((element, index) => <LiveCardItemEmpty key={index}/>)
+            }
+          </div>
         </section>
       </React.Fragment>
     );
